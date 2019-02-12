f62784098c78332114232de22a23d97d67b9e551
JoonasPit <43230082+JoonasPit@users.noreply.github.com>
2019-02-12 11:30:00 +0200

    Update Henkilo.java

--- Henkilo.java
+++ Henkilo.java
@@ -1,17 +1,25 @@
+package lisääläksyjä;
+
 public class Henkilo {
 	
 	private String nimi;
 	private String osoite;
+	private double height;
+	private int weight;
 	
 	public Henkilo() {
 		// sets both name and address to null
 		nimi = "";
 		osoite = "";
+		height = 0;
+		weight = 0;
 	}
-	public Henkilo(String nimi, String osoite) {
+	public Henkilo(String nimi, String osoite, double height, int weight) {
 		// sets name and address to given values
 		this.nimi = nimi;
 		this.osoite = osoite;
+		this.height = height;
+		this.weight = weight;
 	}
 	
 	public void setOsoite(String osoite){
@@ -22,6 +30,12 @@ public class Henkilo {
 		// sets name to given value
 		this.nimi = nimi;
 	}
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	public void setWeight (int weight) {
+		this.weight = weight;
+	}
 	public String getOsoite() {
 		// returns address value
 		return osoite;
@@ -30,9 +44,15 @@ public class Henkilo {
 		// returns name value
 		return nimi;
 	}
+	public double getHeight() {
+		return height;
+	}
+	public int getWeight() {
+		return weight;
+	}
 	public String toString() {
 		// Prints full input as one string
-		return "nimi =" + nimi + "osoite=" + osoite;
+		return "nimi =" + nimi + "osoite=" + osoite + height + weight;
 	}
 
 }
